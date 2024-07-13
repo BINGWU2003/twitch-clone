896c6017e3233d462dc820801a4aa01610ec3184
BINGWU <HuJiaCheng2003@163.com>
2024-07-13 17:53:38 +0800

    feat: Remove unused global CSS file and optimize layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,12 +20,9 @@ import type { Metadata } from "next";
 import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import {
   ClerkProvider
 } from '@clerk/nextjs'
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
