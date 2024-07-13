d91869c79b05e9c7741131dc652b5d81ce70e35b
BINGWU <HuJiaCheng2003@163.com>
2024-07-13 17:40:24 +0800

    refactor: Optimize sidebar component and add lazy loading for recommended users

--- app/(browse)/_components/siderbar/recommended.tsx
+++ app/(browse)/_components/siderbar/recommended.tsx
@@ -1,12 +1,13 @@
 
 import { UserItem } from "./user-item";
 import { getRecommended } from "@/lib/recommended-service"
+import { User } from "@prisma/client";
 export const Recommended = async () => {
   const recommended = await getRecommended()
   return (
     <>
       {
-        recommended.map((user) => {
+        recommended.map((user: User) => {
           return (
             <UserItem user={user} key={user.id} isLive={true}></UserItem>
           )
